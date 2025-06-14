946d2a21f3456a32e80197d4657e0d03d6c97379
Łukasz Grobelczyk <52706905+Lukasz-gro@users.noreply.github.com>
2025-06-14 12:52:58 +0200

    Revert "Add user entites and use cases (#25)" (#27)
    This reverts commit be1ed95ee8a9010afa759373297eeeb1225b74e2.

--- src/shared/application/ports/out/session-repo.ts
+++
@@ -1,8 +0,0 @@
-import { Session } from '@/shared/entities/session';
-
-export interface SessionRepo {
-    create(session: Session): Promise<Session>;
-    findById(id: string): Promise<Session | null>;
-    findByUserId(userId: string): Promise<Session[]>;
-    invalidate(sessionId: string): Promise<void>;
-}
