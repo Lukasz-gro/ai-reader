7dfe86040954d0f340a59f7cba35a6e2155f85cf
Piotr Jasinski <piotr.jasinski990@gmail.com>
2025-07-10 16:49:29 +0200

    fix args to auth controller

--- src/shared/config/auth-setup.ts
+++ src/shared/config/auth-setup.ts
@@ -1,12 +1,12 @@
-import { AxiosHttpClient } from '@/shared/infra/http/axios-http-client';
+import { httpClient } from '@/shared/infra/http/axios-http-client';
 import { HttpCookieAuthProvider } from '@/shared/infra/auth/http-cookie-auth-provider';
 import { LoginUserUseCase } from '@/shared/application/use-cases/login-user';
 import { LogoutUserUseCase } from '@/shared/application/use-cases/logout-user';
 import { GetAuthenticatedUserUseCase } from '@/shared/application/use-cases/get-authenticated-user';
 import { UserAuthController } from '@/shared/interface/controllers/user-auth-controller';
+import { HttpClient } from '@/shared/application/ports/out/http-client';
 
-export const createAuthController = (): UserAuthController => {
-    const httpClient = new AxiosHttpClient();
+export const createAuthController = (httpClient: HttpClient): UserAuthController => {
     const authProvider = new HttpCookieAuthProvider(httpClient);
     
     const loginUserUseCase = new LoginUserUseCase(authProvider);
@@ -20,4 +20,4 @@ export const createAuthController = (): UserAuthController => {
     );
 };
 
-export const authController = createAuthController(); 
+export const authController = createAuthController(httpClient);
