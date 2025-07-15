c8ae5119d60d6fabe471c47e013dff42918b5999
lgrobelczyk <lukaszgrobelczyk@gmail.com>
2025-07-15 19:02:15 +0200

    Add UI components

--- src/shared/interface/web/react/routes/index.tsx
+++ src/shared/interface/web/react/routes/index.tsx
@@ -3,6 +3,8 @@ import { RouteObject } from 'react-router-dom';
 import { ROUTES } from './routePaths';
 import { Home } from './Home';
 import { Quiz } from './Quiz';
+import { Login } from './Login';
+import { Register } from './Register';
 // TODO: Add other route components as they are created
 // import { Chat } from './Chat';
 
@@ -10,6 +12,14 @@ export const routes: RouteObject[] = [
     {
         path: ROUTES.HOME,
         element: <Home />,
+    },
+    {
+        path: ROUTES.LOGIN,
+        element: <Login />,
+    },
+    {
+        path: ROUTES.REGISTER,
+        element: <Register />,
     },
     // TODO: Add more routes as they are implemented
     // {
