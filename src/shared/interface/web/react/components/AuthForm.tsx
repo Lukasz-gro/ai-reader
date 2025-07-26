c0f6b140a55a0ae1dc430edb08a7cf40a41603d0
Piotr Jasinski <piotr.jasinski990@gmail.com>
2025-07-26 14:22:14 +0200

    adjust chat UI

--- src/shared/interface/web/react/components/AuthForm.tsx
+++ src/shared/interface/web/react/components/AuthForm.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const inputFieldClasses = 'block w-full px-3 py-2 border border-p-30 rounded-md shadow-sm placeholder-p-50 focus:outline-none focus:border-sd-70 focus:shadow-lg text-p-90';
 
 export const AuthFormContainer = ({ title, children }: { title: string; children: React.ReactNode }) => (
-    <div className='min-h-screen flex items-center justify-center'>
+    <div className='w-full min-h-screen flex items-center justify-center'>
         <div className='max-w-lg w-full space-y-8 py-8 px-12 bg-p-10 border-4 border-p-90/25 rounded-lg shadow-xl shadow-p-80/30'>
             <h2 className='mt-6 text-center text-p-90 text-3xl font-extrabold'>{title}</h2>
             {children}
